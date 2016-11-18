5d45ddabc2a4c868baa30b907df973a760b6f99b
Kevin Tindall <kevinkjt2000@gmail.com>
2016-11-17 20:22:53 -0600

    Plugin installation is now done after linking and pulling

--- ellipsis.sh
+++ ellipsis.sh
@@ -4,17 +4,18 @@
 
 pkg.install() {
   git submodule update --init --recursive
-  vim +PluginInstall +qall
 }
 
 pkg.link() {
   fs.link_file vimrc
   mkdir -p ~/.vim/bundle
   fs.link_file vim/bundle/Vundle.vim ~/.vim/bundle/Vundle.vim
+  vim +PluginInstall +qall
 }
 
 pkg.pull() {
   git.pull
   git submodule update --recursive --remote
+  vim +PluginInstall +qall
 }
 
