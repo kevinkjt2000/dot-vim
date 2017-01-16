874d30825e9917e2dea9feee0825a4c4cefa9276
Kevin Tindall <kevinkjt2000@gmail.com>
2017-01-16 16:26:10 -0600

    Added a links hook and changed over to ELLIPSIS_HOME

--- ellipsis.sh
+++ ellipsis.sh
@@ -7,11 +7,21 @@ pkg.install() {
 }
 
 pkg.link() {
-	mkdir -p ~/.config/nvim/bundle
-	fs.link_file vimrc ~/.config/nvim/init.vim
-	fs.link_file plugins.vim ~/.config/nvim/plugins.vim
-	fs.link_file vim/bundle/Vundle.vim ~/.config/nvim/bundle/Vundle.vim
+	mkdir -p "$ELLIPSIS_HOME/.config/nvim/bundle"
+	fs.link_file vimrc "$ELLIPSIS_HOME/.config/nvim/init.vim"
+	fs.link_file plugins.vim "$ELLIPSIS_HOME/.config/nvim/plugins.vim"
+	fs.link_file vim/bundle/Vundle.vim "$ELLIPSIS_HOME/.config/nvim/bundle/Vundle.vim"
 	nvim +PluginInstall +qall
+}
+
+pkg.links() {
+	msg.bold "${1:-$PKG_NAME}"
+	local files="init.vim plugins.vim bundle/Vundle.vim"
+	for file in $files; do
+		local file="$ELLIPSIS_HOME/.config/nvim/$file"
+		local link="$(readlink "$file")"
+		echo "$(path.relative_to_packages "$link") -> $(path.relative_to_home "$file")";
+	done
 }
 
 pkg.pull() {
