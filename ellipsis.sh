4f1b6ce19e87643ec07f1ed8a21234edfaf4f30b
Kevin Tindall <kevinkjt2000@gmail.com>
2017-01-16 16:05:58 -0600

    neovim seems to have a good future; goodbye vim

--- ellipsis.sh
+++ ellipsis.sh
@@ -3,33 +3,21 @@
 # kevinkjt2000/vim ellipsis package
 
 pkg.install() {
-  git submodule update --init --recursive
+	git submodule update --init --recursive
 }
 
 pkg.link() {
-  if utils.cmd_exists nvim; then
-    mkdir -p ~/.config/nvim
-    fs.link_file vimrc ~/.config/nvim/init.vim
-    fs.link_file plugins.vim ~/.config/nvim/plugins.vim
-    mkdir -p ~/.config/nvim/bundle
-    fs.link_file vim/bundle/Vundle.vim ~/.config/nvim/bundle/Vundle.vim
-    nvim +PluginInstall +qall
-  else
-    fs.link_file vimrc
-    fs.link_file plugins.vim
-    mkdir -p ~/.vim/bundle
-    fs.link_file vim/bundle/Vundle.vim ~/.vim/bundle/Vundle.vim
-    vim +PluginInstall +qall
-  fi
+	mkdir -p ~/.config/nvim
+	fs.link_file vimrc ~/.config/nvim/init.vim
+	fs.link_file plugins.vim ~/.config/nvim/plugins.vim
+	mkdir -p ~/.config/nvim/bundle
+	fs.link_file vim/bundle/Vundle.vim ~/.config/nvim/bundle/Vundle.vim
+	nvim +PluginInstall +qall
 }
 
 pkg.pull() {
-  git.pull
-  git submodule update --recursive --remote
-  if utils.cmd_exists nvim; then
-    nvim +PluginInstall +qall
-  else
-    vim +PluginInstall +qall
-  fi
+	git.pull
+	git submodule update --recursive --remote
+	nvim +PluginInstall +qall
 }
 
